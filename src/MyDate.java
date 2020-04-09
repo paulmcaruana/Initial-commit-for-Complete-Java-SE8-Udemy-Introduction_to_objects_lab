4ffe1a0614bdb5cec54b2df20150bb2662711259
Paul <paul@beambient.co.uk>
2020-04-09 15:28:17 +0100

    Final commit of working lab

--- src/MyDate.java
+++ src/MyDate.java
@@ -1,4 +1,24 @@
 
 public class MyDate {
 
+	int day;
+	int year;
+	int month;
+	
+	public MyDate () {}
+	
+	public MyDate (int m, int d, int y) {
+		setDate(m, d, y);
+	}
+	
+	public String toString(){
+		return month + "/" + day + "/" + year;
+	}
+	
+public void setDate(int m, int d, int y) {
+	day = d;
+	year = y;
+	month = m;
+}
+	
 }
